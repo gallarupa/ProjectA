47e5854e89fe0d78b4a668a060438ff3e79eb3c0
gallarupa <rupagalla02@gmail.com>
2024-09-10 22:10:50 +0530

    recent changes

--- src/test/java/org/test/SampleG.java
+++ src/test/java/org/test/SampleG.java
@@ -1,5 +1,8 @@
 package org.test;
 
 public class SampleG {
-
+    public static void main(String[] args) {
+    	System.out.println("test");
+       //here start to write implementations	
+    }
 }
